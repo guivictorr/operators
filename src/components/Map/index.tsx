c1e6963043a027fca4823b8274b401fc4953d17f
guivictorr <guilhermevictor20@gmail.com>
2021-09-05 15:29:02 -0300

    feat(Marker): linked marker to pages

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import * as L from 'leaflet';
 
@@ -18,34 +19,41 @@ type MapProps = {
   places?: Place[];
 };
 
-const Map = ({ places }: MapProps) => (
-  <MapContainer
-    center={[0, 0]}
-    zoom={3}
-    style={{ height: '100%', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+const Map = ({ places }: MapProps) => {
+  const router = useRouter();
 
-    {places?.map(({ name, location, id, icon }) => {
-      const { latitude, longitude } = location;
-      const customIcon = L.icon({
-        iconUrl: icon.url,
-        iconSize: [40, 40],
-        iconAnchor: [20, 20]
-      });
-      return (
-        <Marker
-          title={name}
-          key={id}
-          position={[latitude, longitude]}
-          icon={customIcon}
-        />
-      );
-    })}
-  </MapContainer>
-);
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {places?.map(({ name, location, id, icon, slug }) => {
+        const { latitude, longitude } = location;
+        const customIcon = L.icon({
+          iconUrl: icon.url,
+          iconSize: [40, 40],
+          iconAnchor: [20, 20]
+        });
+        return (
+          <Marker
+            title={name}
+            key={id}
+            position={[latitude, longitude]}
+            icon={customIcon}
+            eventHandlers={{
+              click: () => router.push(`/${slug}`)
+            }}
+          />
+        );
+      })}
+    </MapContainer>
+  );
+};
 
 export default Map;
