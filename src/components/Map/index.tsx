7f3c4fafa63d32aa55e1898edf188ac8a2a39f85
guivictorr <guilhermevictor20@gmail.com>
2021-09-06 19:26:29 -0300

    feat(Map): changed background color

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import * as L from 'leaflet';
-
+import * as S from './styles';
 export type Place = {
   id: string;
   name: string;
@@ -23,41 +23,43 @@ const Map = ({ places }: MapProps) => {
   const router = useRouter();
 
   return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={3}
-      minZoom={3}
-      maxBounds={[
-        [-180, 180],
-        [180, -180]
-      ]}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <TileLayer
-        attribution='© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url={`https://api.mapbox.com/styles/v1/${process.env.NEXT_PUBLIC_MAPBOX_USER_ID}/${process.env.NEXT_PUBLIC_MAPBOX_STYLE_ID}/tiles/{z}/{x}/{y}?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
-      />
+    <S.MapWrapper>
+      <MapContainer
+        center={[0, 0]}
+        zoom={3}
+        minZoom={3}
+        maxBounds={[
+          [-180, 180],
+          [180, -180]
+        ]}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url={`https://api.mapbox.com/styles/v1/${process.env.NEXT_PUBLIC_MAPBOX_USER_ID}/${process.env.NEXT_PUBLIC_MAPBOX_STYLE_ID}/tiles/{z}/{x}/{y}?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
+        />
 
-      {places?.map(({ name, location, id, icon, slug }) => {
-        const { latitude, longitude } = location;
-        const customIcon = L.icon({
-          iconUrl: icon.url,
-          iconSize: [40, 40],
-          iconAnchor: [20, 20]
-        });
-        return (
-          <Marker
-            title={name}
-            key={id}
-            position={[latitude, longitude]}
-            icon={customIcon}
-            eventHandlers={{
-              click: () => router.push(`/${slug}`)
-            }}
-          />
-        );
-      })}
-    </MapContainer>
+        {places?.map(({ name, location, id, icon, slug }) => {
+          const { latitude, longitude } = location;
+          const customIcon = L.icon({
+            iconUrl: icon.url,
+            iconSize: [40, 40],
+            iconAnchor: [20, 20]
+          });
+          return (
+            <Marker
+              title={name}
+              key={id}
+              position={[latitude, longitude]}
+              icon={customIcon}
+              eventHandlers={{
+                click: () => router.push(`/${slug}`)
+              }}
+            />
+          );
+        })}
+      </MapContainer>
+    </S.MapWrapper>
   );
 };
 
